269bbd6d27bed3c9f91318cb1369650405708374
Cole Mickens <cole.mickens@gmail.com>
2023-05-11 03:04:14 -0600

    vf2: kernel tweaks

--- pkgs/linux-visionfive2/default.nix
+++ pkgs/linux-visionfive2/default.nix
@@ -1,30 +1,39 @@
 { lib, stdenv, fetchFromGitHub, buildLinux, ... } @ args:
 
+let
+  # last updated: 14-May-2023: https://github.com/starfive-tech/linux/tree/JH7110_VisionFive2_upstream
+  version = "6.4-rc1";
+  rev = "5067e82028046501daa6e0c53e8c54343a217f45";
+  hash = "sha256-rydyJbSCsGZSjyL9r6D/HAj4VePssKM9GGE0834dBeI=";
+in
 buildLinux
   (args // {
     # https://github.com/starfive-tech/linux/tree/JH7110_VisionFive2_upstream
-    version = "6.4-rc1";
+    inherit version;
 
     src = fetchFromGitHub {
       owner = "starfive-tech";
       repo = "linux";
-      rev = "5067e82028046501daa6e0c53e8c54343a217f45";
-      hash = "sha256-rydyJbSCsGZSjyL9r6D/HAj4VePssKM9GGE0834dBeI=";
+      inherit rev hash;
     };
 
     # defconfig = "starfive_visionfive2_defconfig";
-    defconfig = "defconfig";
+    # defconfig = "defconfig";
+    defconfig = "jh7110_defconfig";
 
     structuredExtraConfig = with lib.kernel; {
-      SERIAL_8250_DW = yes;
-      PINCTRL_STARFIVE = yes;
+      # SERIAL_8250_DW = yes;
+      # PINCTRL_STARFIVE = yes;
 
-      # Doesn't build as a module
-      DW_AXI_DMAC_STARFIVE = yes;
+      # TODO: maybe report? see if still an issue after next update
+      PL330_DMA = no;
 
-      # stmmac hangs when built as a module
-      PTP_1588_CLOCK = yes;
-      STMMAC_ETH = yes;
-      STMMAC_PCI = yes;
+      # # Doesn't build as a module
+      # DW_AXI_DMAC_STARFIVE = yes;
+
+      # # stmmac hangs when built as a module
+      # PTP_1588_CLOCK = yes;
+      # STMMAC_ETH = yes;
+      # STMMAC_PCI = yes;
     };
   }) // (args.argsOverride or { })
